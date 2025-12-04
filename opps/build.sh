2618606515a786efdf5dae224eedff466594454d
sambux1 <sambuxbaum01@gmail.com>
2025-12-03 21:58:12 -0500

    cscml final draft

--- opps/build.sh
+++ opps/build.sh
@@ -1,6 +1,9 @@
 # pass name of presentation as command-line argument
 NAME=$1
 
+# generate a PPTX version (with speaker notes)
+slidev export --format pptx --output $NAME.pptx
+
 # generate a PNG for each slide
 slidev export --format png --output build                      # no clicks
 slidev export --with-clicks --format png --output build-clicks # clicks
